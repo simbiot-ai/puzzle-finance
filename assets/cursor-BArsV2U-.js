8a2bda60fd6143ac536911377f63e659c5126c28
Alex Shkor <alex.shkor@gmail.com>
2025-11-21 10:46:53 +0300

    Updates

--- assets/cursor-BArsV2U-.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-C00BqcWP.js";import"./index-CUe2EHv2.js";import"./index.es-D3O8LN_2.js";import"./index-nibyPLVP.js";import"./fallback-BDO91rGM.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
